61816fbf017c17e5965c14b0a85a274182243e73
frasergr <frasergraham@gmail.com>
2017-08-29 09:35:08 -0700

    API controller; Guzzle dependency; .env.example for API

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class ApiController extends Controller
+{
+    /**
+     * Handles GET requests to provider API
+     *
+     * @param $api
+     * @param $params
+     * @return mixed
+     */
+    public function providerGetApi($api, $params)
+    {
+        $params['format'] = 'json';
+        $client = new Client();
+        try {
+            $response = $client->request('GET',env('R_API_HOST') . $api, [
+                'headers' => [
+                    'merchant_fid' => env('R_API_MERCHANT_FID'),
+                    'api_key' => env('R_API_KEY'),
+                    'version' => env('R_API_VERSION'),
+                ],
+                'query' => http_build_query($params)
+            ]);
+        } catch (GuzzleException $e) {
+            if ($e->hasResponse()) {
+                $error = array(
+                    'api' => $api,
+                    'error' => json_decode($e->getResponse()->getBody()->getContents(), true)['response']['message']
+                );
+//                ExceptionController::emailError($subject = "Invalid R_API '" . $api . "' Request", $error);
+                return response($error, 400)->throwResponse();
+            }
+        }
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Handles PUT requests to provider API
+     *
+     * @param $api
+     * @param $params
+     * @return mixed
+     */
+    public function providerPutApi($api, $params)
+    {
+        $params['format'] = 'json';
+        $client = new Client();
+        try {
+            $response = $client->request('PUT',env('R_API_HOST') . $api, [
+                'headers' => [
+                    'merchant_fid' => env('R_API_MERCHANT_FID'),
+                    'api_key' => env('R_API_KEY'),
+                    'version' => env('R_API_VERSION'),
+                ],
+                'form_params' => $params
+            ]);
+            return json_decode($response->getBody(), true);
+        } catch (GuzzleException $e) {
+            $error = array(
+                'api' => $api,
+                'error' => json_decode($e->getResponse()->getBody()->getContents(), true)['response']['message']
+            );
+//            ExceptionController::emailError($subject = "Invalid R_API '" . $api . "' Request", $error);
+            return response($error, 400)->throwResponse();
+        }
+    }
+
+    /**
+     * Handles POST requests to provider API
+     *
+     * @param $api
+     * @param $params
+     * @return mixed
+     */
+    public function providerPostApi($api, $params)
+    {
+        $params['format'] = 'json';
+        $client = new Client();
+        try {
+            $response = $client->request('POST',env('R_API_HOST') . $api, [
+                'headers' => [
+                    'merchant_fid' => env('R_API_MERCHANT_FID'),
+                    'api_key' => env('R_API_KEY'),
+                    'version' => env('R_API_VERSION'),
+                ],
+                'form_params' => $params
+            ]);
+            return json_decode($response->getBody(), true);
+        } catch (GuzzleException $e) {
+            $error = array(
+                'api' => $api,
+                'error' => json_decode($e->getResponse()->getBody()->getContents(), true)['response']['message']
+            );
+//            ExceptionController::emailError($subject = "Invalid R_API '" . $api . "' Request", $error);
+            return response($error, 400)->throwResponse();
+        }
+    }
+}
