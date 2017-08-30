b01701c023ab952757325d61d122e69a11caf759
frasergr <frasergraham@gmail.com>
2017-08-30 11:34:31 -0700

    Modified .env keys for ITN validation; force HTTPS on production

--- app/Http/Middleware/ValidateItn.php
+++ app/Http/Middleware/ValidateItn.php
@@ -15,8 +15,8 @@ class ValidateItn
      */
     public function handle($request, Closure $next)
     {
-        $xmlSignature = $request->hasHeader(env('R_ITN_SIG')) ? $request->header(env('R_ITN_SIG')) : null;
-        $contentSignature = base64_encode(hash_hmac('sha256', $request->getContent(), env('A_ITN_SECRET')));
+        $xmlSignature = $request->hasHeader(env('R_ITN_SIG')) ? $request->header(env('ITN_SIG')) : null;
+        $contentSignature = base64_encode(hash_hmac('sha256', $request->getContent(), env('ITN_SECRET')));
 
         if ($xmlSignature !== $contentSignature) {
             return response('Not Authorized', 500);
